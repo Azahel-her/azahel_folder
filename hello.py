997f248468c05632a9e0afbd977ac6a24129315f
Azahel-her <azahel.pelo@gmail.com>
2024-08-30 10:38:51 -0600

    I upload this program

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("What is your firstName and lastName")
+firstName=input()
+lastName=input()
+print("it is good to meet you, " + firstName + ', ' + lastName)
+      
+
+
+
+
---
+++ hello.py2.py
@@ -0,0 +1,8 @@
+#Calculate the area of a circle
+PI = 3.14
+print("This program calculates the area of a circle.please enter the radius of the circle")
+radius=input()
+radius=float(radius)
+area = PI * radius * radius
+print("the area of your circle is=" + str(area))
+
