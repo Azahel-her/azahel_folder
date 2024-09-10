c638c31ad4ed398752f2b9b737496e461e5c3792
Azahel-her <azahel.pelo@gmail.com>
2024-09-10 13:55:05 -0600

    new file

--- azanames.py3.py
+++ azanames.py3.py
@@ -1,7 +1,14 @@
-first_name = input("first :")
-last_name =  input("last :")
-count = len(first_name+ last_name)
-initials = first_name[0]+last_name[0]
-doublefirstname= first_name[0]*2+first_name[1]*2+first_name[2]*2 + first_name[3]*2 + first_name[4]*2+first_name[5]*2
-spelledbackward=last_name[9]+last_name[8]+last_name[7]+last_name[6]+last_name[5]+last_name[4]+last_name[3]+last_name[2]+last_name[1]
-print (f"Hello {last_name},{first_name} ! Your name has {count} letters, and your initials are {initials}. {doublefirstname} is repeated, your name spelled backward is {spelledbackward}")
+userfname= input("Fname:")
+userlname= input("Lname:")
+
+longstr = len(userfname + userlname)
+
+initias = userfname[0] + userlname[0]
+
+double_fname = ""
+for letter in userfname:
+    double_fname += letter + letter
+
+revered_name = userlname[::-1]
+print(revered_name)
+print(f"Hello {userlname}, {userfname}! Your name has {longstr} letters, and your initials are {initias}. {double_fname}, your name spelled backwards is {revered_name}.")
