540a961a7a3aaf677075d96a39c31aac05c85a81
Azahel-her <azahel.pelo@gmail.com>
2024-10-17 11:16:53 -0600

    hola:)

---
+++ test.py
@@ -0,0 +1,18 @@
+a = ["master", "online"]
+a.insert(1,"code")
+print(a)
+c =[1,2,3]
+c =[4,5,6]
+d = [1,2,3]
+c.insert(0,d)
+print(c)
+
+names = ["Donogay","Azagay","santigay"]
+names.sort()
+names.sort(reverse=True)
+names.sort(reverse=True, key=len)
+
+print(names)
+
+
+
